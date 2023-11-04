1be9f641456e5a250a645738691961884e039754
jannatsoftit <jannatlamitlogiko@gmail.com>
2023-11-05 00:42:29 +0600

    -fixed all users show function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -48,73 +48,6 @@ use Illuminate\Support\Facades\Hash;
 class AdminController extends Controller
 {
 
-  public function profile(AdminRequest $request)
-  {
-    $validation = $request->validated();
-    $user = User::where('email', $validation->email)->first();
-
-    if (!$user || !Hash::check($validation->password, $user->password)) {
-      return response()->json([
-        'status' => 401,
-        'message' => 'Invalid Credentials.'
-      ]);
-    } else {
-      $user_info = User::where('id', '1')->value('user_information');
-      //$photo = json_decode($user_info)->photo;
-      $gender = json_decode($user_info)->gender;
-      $birthday = json_decode($user_info)->birthday;
-      $address = json_decode($user_info)->address;
-      return response()->json([
-        'status' => 200,
-        'username' => $user->name,
-        'userEmail' => $user->email,
-        'gender' => $gender,
-        'birthday' => $birthday,
-        'address' => $address,
-        'role_id' => $user->role_id,
-        'message' => 'Profile show successfull.'
-      ]);
-    }
-  }
-
-
-  // public function profile_update(User $request, $id)
-  // {
-  //   $data = $request->all();
-
-  //   if (!empty($data['photo'])) {
-  //     $file = $data['photo'];
-  //     $filename = time() . '-' . $file->getClientOriginalExtension();
-  //     $file->move('admin-images/', $filename);
-  //     $photo = $filename;
-  //   } else {
-  //     $user_info = User::where('id', $id)->value('user_information');
-  //     $exsisting_filename = json_decode($user_info)->photo;
-  //     if ($exsisting_filename !== '') {
-  //       $photo = $exsisting_filename;
-  //     } else {
-  //       $photo = '';
-  //     }
-  //   }
-  //   $user_info = User::where('id', $id)->value('user_information');
-
-  //   $info = array(
-  //     'gender' => $data['gender'],
-  //     'blood_group' => json_decode($user_info)->blood_group,
-  //     'birthday' => date($data['birthday']),
-  //     'phone' => $data['phone'],
-  //     'address' => $data['address'],
-  //     'photo' => $photo
-  //   );
-
-  //   $data['user_information'] = json_encode($info);
-  //   User::where('id', $id)->update([
-  //     'name' => $data['name'],
-  //     'email' => $data['email'],
-  //     'user_information' => $data['user_information']
-  //   ]);
-  //   return redirect()->route('admin.admin')->with('success', 'Profile Updated Successfully');
-  // }
   public function admin_list(Request $request): JsonResponse
   {
     return response()->json([
@@ -313,6 +246,7 @@ class AdminController extends Controller
 
   public function student_Show(User $student)
   {
+    $student->user_information = json_decode($student->user_information);
     return response()->json([
       'data' => [
         'student' => $student,
@@ -420,6 +354,7 @@ class AdminController extends Controller
 
   public function parent_Show(User $parent)
   {
+    $parent->user_information = json_decode($parent->user_information);
     return response()->json([
       'data' => [
         'parent' => $parent,
@@ -519,6 +454,7 @@ class AdminController extends Controller
 
   public function teacher_show(User $teacher)
   {
+    $teacher->user_information = json_decode($teacher->user_information);
     return response()->json([
       'data' => [
         'teacher' => $teacher,
